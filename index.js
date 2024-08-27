f7b21c221f84c1656af2d9b46d65cb17787de029
Gaurav Sharma <gaurav@o7services.com>
2024-08-27 15:51:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('<h2>My Name is Node JS Server</h2>')
 })
+app.get('/about', function (req, res) {
+    res.json({
+        "name":"Gaurav",
+        "email":"a@a.com",
+        "phone":"9987877766"
+    })
+  })
 
 app.listen(8080,function(){
     console.log("Server is running at 3000")
